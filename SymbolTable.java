ba8bab7144cac437d37523418e4dabb02dec8fcb
Ayub Ali <97268297+yubiszn@users.noreply.github.com>
2024-09-28 09:26:57 -0400

    Create SymbolTable.java

---
+++ SymbolTable.java
@@ -0,0 +1,12 @@
+public class SymbolTable {
+
+    public void setValue(String name, int value) {
+    }
+
+    public Integer getValue(String name) {
+        return null;
+    }
+
+    private static class SymbolEntry {
+    }
+}
